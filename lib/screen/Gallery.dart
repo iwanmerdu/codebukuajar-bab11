6a6beb8ad872c0afc58340d14fb3bb898203a180
Ridwan, M.T <92246014+iwanmerdu@users.noreply.github.com>
2021-10-14 20:58:25 +0700

    Add files via upload

---
+++ lib/screen/Gallery.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Gallery extends StatelessWidget {
+  const Gallery
+({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Halaman Gallery'),
+      ),
+    );
+  }
+}
---
+++ lib/screen/Mahasiswa.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Mahasiswa extends StatelessWidget {
+  const Mahasiswa({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Halaman Gallery'),
+      ),
+    );
+  }
+}
